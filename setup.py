eeb785cbd0c89ebc327589a797277d68c6a7b551
Dmytro Upolovnikov <dmitryup@gmail.com>
2016-09-30 12:08:22 +0300

    updated aiohttp to stable version

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
-from pip.req import parse_requirements
 
 
-install_reqs = parse_requirements('requirements.txt', session=False)
-reqs = [str(ir.req) for ir in install_reqs]
-
-setup(name='plankton',
-      version='0.1',
-      description='Plankton - wkhtmltopdf REST service',
-      url='https://github.com/django-stars/plankton',
-      author='Dmytro Upolovnikov',
-      author_email='dmitry.upolovnikov@djangostars.com',
-      license='Apache License 2.0',
-      packages=['plankton', 'plankton.wkhtmltopdf'],
-      scripts=['scripts/plankton-server'],
-      install_requires=reqs,
-      zip_safe=False)
+setup(
+    name='plankton',
+    version='0.1.1',
+    description='Plankton - wkhtmltopdf REST service',
+    url='https://github.com/django-stars/plankton',
+    author='Dmytro Upolovnikov',
+    author_email='dmitry.upolovnikov@djangostars.com',
+    license='Apache License 2.0',
+    packages=['plankton', 'plankton.wkhtmltopdf'],
+    scripts=['scripts/plankton-server'],
+    download_url = 'https://github.com/django-stars/plankton/tarball/0.1.1',
+    keywords=['html to pdf', 'wkhtmltopdf', 'pdf', 'rest'],
+    install_requires=['aiohttp==1.0.2', 'Cerberus==0.9.2', 'validators==0.10.1', 'requests==2.10.0'],
+    zip_safe=False
+)

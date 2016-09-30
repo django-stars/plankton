83a672d8d40f8bea51dc772aa084139c409d47e4
Dmytro Upolovnikov <dmitryup@gmail.com>
2016-09-30 15:47:13 +0300

    Added license

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     url='https://github.com/django-stars/plankton',
     author='Dmytro Upolovnikov',
     author_email='dmitry.upolovnikov@djangostars.com',
-    license='Apache License 2.0',
+    license='MIT',
     packages=['plankton', 'plankton.wkhtmltopdf'],
     scripts=['scripts/plankton-server'],
-    download_url = 'https://github.com/django-stars/plankton/tarball/0.1.2',
+    download_url='https://github.com/django-stars/plankton/tarball/0.1.2.1',
     keywords=['html to pdf', 'wkhtmltopdf', 'pdf', 'rest'],
     install_requires=['aiohttp==1.0.2', 'Cerberus==0.9.2', 'validators==0.10.1', 'requests==2.10.0'],
     zip_safe=False

c9552b1875c1c0e6705f51f82c7f2bee441fab7f
Dmytro Upolovnikov <dmitryup@gmail.com>
2016-09-30 12:33:35 +0300

    pypi interation

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license='Apache License 2.0',
     packages=['plankton', 'plankton.wkhtmltopdf'],
     scripts=['scripts/plankton-server'],
-    download_url = 'https://github.com/django-stars/plankton/tarball/0.1.1',
+    download_url = 'https://github.com/django-stars/plankton/tarball/0.1.2',
     keywords=['html to pdf', 'wkhtmltopdf', 'pdf', 'rest'],
     install_requires=['aiohttp==1.0.2', 'Cerberus==0.9.2', 'validators==0.10.1', 'requests==2.10.0'],
     zip_safe=False
